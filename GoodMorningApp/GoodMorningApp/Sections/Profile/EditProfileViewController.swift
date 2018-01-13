6fa0d3baea8a7f39ab897fbee3f6aacd096398f6
= <renan.rsg@hotmail.com>
2018-01-13 02:27:13 -0200

    Correções de Storyboard

--- GoodMorningApp/GoodMorningApp/Sections/Profile/EditProfileViewController.swift
+++ GoodMorningApp/GoodMorningApp/Sections/Profile/EditProfileViewController.swift
@@ -1,39 +1,44 @@
 //
-//  PorfileViewController.swift
+//  EditProfileViewController.swift
 //  GoodMorningApp
 //
-//  Created by Renan Soares Germano on 08/01/2018.
+//  Created by Renan Soares Germano on 13/01/2018.
 //  Copyright © 2018 Renan Soares Germano. All rights reserved.
 //
 
 import UIKit
 
-class PorfileViewController: UIViewController {
+class EditProfileViewController: UIViewController {
     
     //MARK: Outlets
-    @IBOutlet weak var userName: UILabel!
+    @IBOutlet weak var userName: UITextField!
     @IBOutlet weak var userPhoto: UIImageView!
-    @IBOutlet weak var userEmail: UILabel!
+    @IBOutlet weak var userEmail: UITextField!
     @IBOutlet weak var userAbout: UITextView!
-    @IBOutlet weak var userContact: UILabel!
+    @IBOutlet weak var userContact: UITextField!
     
     //MARK: Properties
     var user: User?
-
-    //MARK: Life sicle functions
+    
+    //MARK: Life cicle functions
     override func viewDidLoad() {
         super.viewDidLoad()
         if let user = self.user {
-            userName.text = user.name!
-            userEmail.text = user.email!
-            userAbout.text = user.about!
-            userContact.text = user.contacts.first?.content ?? "Adcione um contato."
+            self.userName.text = user.name!
+//            self.userPhoto
+            self.userEmail.text = user.email!
+            self.userAbout.text = user.about!
+            self.userContact.text = user.contacts.first?.content ?? "Adicione um contato"
         }
     }
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
+    }
+    
+    //MARK: Actions
+    @IBAction func userPhotoTapped(_ sender: UITapGestureRecognizer) {
     }
     
 
