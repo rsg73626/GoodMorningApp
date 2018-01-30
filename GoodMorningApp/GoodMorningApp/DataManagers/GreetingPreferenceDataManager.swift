9dc7a74d8971b932f5460a5a17e3df49e12cb69d
= <renan.rsg@hotmail.com>
2018-01-30 14:08:17 -0200

    Adding new assets.

---
+++ GoodMorningApp/GoodMorningApp/DataManagers/GreetingPreferenceDataManager.swift
@@ -0,0 +1,9 @@
+//
+//  GreetingPreferenceDataManager.swift
+//  GoodMorningApp
+//
+//  Created by Renan Soares Germano on 30/01/2018.
+//  Copyright © 2018 Renan Soares Germano. All rights reserved.
+//
+
+import Foundation
